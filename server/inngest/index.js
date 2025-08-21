1d7f8e7990e064456c67f1ef43df2d4356d0bab5
sanjay-2684Coder <sanjaydangi9294@gmail.com>
2025-08-21 23:54:10 +0530

    model import fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,5 @@
 import {Inngest} from "inngest";
-import User from "../models/User";
+import User from "../models/User.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
@@ -12,7 +12,7 @@ const syncUserCreation  = inngest.createFunction(
         const {id,first_name,last_name,email_addresses,image_url} = event.data
         const userData = {
             _id:id,
-            email : email_addresses[0].email_addresses,
+            email : email_addresses[0].email_address,
             name:first_name + ' ' + last_name,
             image : image_url
         }
@@ -39,7 +39,7 @@ const syncUserUpdation  = inngest.createFunction(
         const {id,first_name,last_name,email_addresses,image_url} = event.data
         const userData = {
             _id: id,
-            email : email_addresses[0].email_addresses,
+            email : email_addresses[0].email_address,
             name:first_name + ' ' + last_name,
             image : image_url
         }
